8f3ffaeb07eba5e64a831bd67beb974881b6f261
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2025-01-06 21:07:24 -0300

    update

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -1,22 +1,26 @@
 import { FastifyInstance } from "fastify";
-import { prisma } from "../lib/prisma";
-import { z } from "zod";
 import { createReadStream } from "node:fs";
+import { z } from "zod";
 import { openai } from "../lib/openai";
+import { prisma } from "../lib/prisma";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
-  app.post("/videos/:videoId/transcription", async (req) => {
+app.post("/videos/:videoId/transcription", async (req) => {
+    console.log("Iniciando a rota de transcrição...");
+
     const paramsSchema = z.object({
       videoId: z.string().uuid(),
     });
 
     const { videoId } = paramsSchema.parse(req.params);
+    console.log(`ID do vídeo: ${videoId}`);
 
     const bodySchema = z.object({
-      prompt: z.string(),
+      prompt: z.string().default(''),
     });
 
     const { prompt } = bodySchema.parse(req.body);
+    console.log(`Prompt recebido: ${prompt}`);
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -25,29 +29,36 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     });
 
     const videoPath = video.path;
+    console.log(`Caminho do vídeo: ${videoPath}`);
 
     const audioReadStream = createReadStream(videoPath);
 
-    const response = await openai.audio.transcriptions.create({
-      file: audioReadStream,
-      model: "whisper-1",
-      language: "pt",
-      response_format: "json",
-      temperature: 0,
-      prompt,
-    });
+    try {
+      const response = await openai.audio.transcriptions.create({
+        file: audioReadStream,
+        model: "whisper-1",
+        language: "pt",
+        response_format: "json",
+        temperature: 0,
+        prompt,
+      });
 
-    const transcription = response.text;
+      const transcription = response.text;
+      console.log("Transcrição recebida com sucesso.");
 
-    await prisma.video.update({
-      where: {
-        id: videoId,
-      },
-      data: {
-        transcription,
-      },
-    });
+      await prisma.video.update({
+        where: {
+          id: videoId,
+        },
+        data: {
+          transcription,
+        },
+      });
 
-    return transcription;
+      return transcription;
+    } catch (error) {
+      console.error("Erro ao chamar a API da OpenAI:", error);
+      throw new Error("Erro ao processar a transcrição.");
+    }
   });
 }
